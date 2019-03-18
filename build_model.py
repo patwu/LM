aeb6160d2664c9661fbf7f688ddf3fc056fa56bd
a <a@b.c>
2019-03-18 16:46:27 +0800

    corpus list

--- build_model.py
+++ build_model.py
@@ -2,25 +2,31 @@ import sys
 import argparse
 import json
 import numpy as np
+import os
 
 def build_model():
     chars={'<BOS>':0,'<EOS>':1}
     sentences=[]
-    with open(args.corpus,'r') as f:
-        lines=f.readlines()
-        for line in lines:
-            data=json.loads(line)
-            text=data['text']
-            sen=text.split('\n')
-            for s in sen:
-                if len(s.strip())>0:
-                    sentences.append(s)
-                    for c in s:
-                        if not c in chars:
-                            chars[c]=len(chars)
+    files=os.listdir(args.corpus)
+    print files
+    for file_ in files:
+        al=os.path.join(args.corpus,file_)
+        print al
+        with open(al,'r') as f:
+            lines=f.readlines()
+            for line in lines:
+                data=json.loads(line)
+                text=data['text']
+                sen=text.split('\n')
+                for s in sen:
+                    if len(s.strip())>0:
+                        sentences.append(s)
+                        for c in s:
+                            if not c in chars:
+                                chars[c]=len(chars)
     print len(chars)
-    c_matrix=np.zeros((4000,4000))
-    p_matrix=np.zeros((4000,4000))
+    c_matrix=np.zeros((7000,7000))
+    p_matrix=np.zeros((7000,7000))
 
     #build language model
     for s in sentences:
@@ -34,7 +40,7 @@ def build_model():
             #count c matrix
             c_matrix[i1,i2]+=1
     #build p matrix
-    for i in range(4000):
+    for i in range(7000):
         sum_=np.sum(c_matrix[i,:])
         p_matrix[i]=c_matrix[i]/sum_
    
@@ -81,8 +87,8 @@ def maximun_generate(model):
             i1=chars[segments[-1]]
             while i1!=1 and len(str_)<50:
                 while True:
-                    i2=np.random.choice(range(4000),p=p_mat[i1])
-                    if p_mat[i1,i2]>0.01:
+                    i2=np.random.choice(range(7000),p=p_mat[i1])
+                    if p_mat[i1,i2]>0.015:
                         break;
                 c2=rchars[i2]
                 str_+=c2
@@ -91,7 +97,7 @@ def maximun_generate(model):
 
 if __name__=='__main__':
     argparser = argparse.ArgumentParser(sys.argv[0])
-    argparser.add_argument('--corpus',type=str,default='corpus/wiki_00')
+    argparser.add_argument('--corpus',type=str,default='corpus')
     argparser.add_argument('--testfile',type=str,default='test.txt')
     argparser.add_argument('--prefix_file', type=str,default='prefix.txt')
     args = argparser.parse_args()
